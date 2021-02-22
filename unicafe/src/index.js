b51e08f9a28c750416408337f104be7c14d4efa5
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-02-22 22:08:05 +0700

    Step 2 done

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({onClick,text}) => (
+  <button onClick = {onClick}>
+  {text}
+  </button>
+)
+
+const Header = ({header}) => {
+  return(
+    <div>
+    <h1> {header} </h1>
+    </div>
+  )
+}
+
+const Display = ({type, stats}) => {
+  return(
+    <div>
+    <p> {type} {stats} </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  const calcTotal = () => good + neutral + bad
+  const calcAverage = () => (good - bad) / calcTotal()
+  const calcPositive = () => good / calcTotal()
+
+  return (
+    <div>
+    <Header header = {'Give Feedback'} />
+    <Button onClick = {handleGood} text = 'Good' />
+    <Button onClick = {handleNeutral} text = 'Neutral' />
+    <Button onClick = {handleBad} text = 'Bad' />
+    <Header header = {'Statistics'} />
+    <Display type = {'Good'} stats = {good} />
+    <Display type = {'Neutral'} stats = {neutral} />
+    <Display type = {'Bad'} stats = {bad} />
+    <Display type = {'All'} stats = {calcTotal()} />
+    <Display type = {'Average'} stats = {calcAverage()} />
+    <Display type = {'Positive'} stats = {calcPositive()} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
