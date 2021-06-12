78b286046ee4bc73cb42762cb0e4f80a9185bb5d
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-12 23:57:17 +0700

    Implemented allBooks query

--- library/backend.js
+++ library/backend.js
@@ -79,16 +79,24 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Books {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Books!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
