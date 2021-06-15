395d2076cdd5eb658ca8963a23857e3fa4013eea
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-15 22:59:19 +0700

    Added a test for multiple blogs

--- bloglist/tests/totalLikes.test.js
+++ bloglist/tests/totalLikes.test.js
@@ -1,7 +1,7 @@
 const totalLikes = require("../utils/list_helper")
 
 describe("Total Likes", () => {
-    const blogs = [{
+    const listWithSingleBlog = [{
         "title": "A Day in a Life of a CS Student",
         "author": "Sergio Vieri",
         "url": "http://sergiovieri.diary.com",
@@ -9,7 +9,33 @@ describe("Total Likes", () => {
     }]
 
     test("When the list has only one blog, the total likes is equal to the blog's likes", () => {
-        const result = totalLikes(blogs)
-        expect(result).toBe(150)
+        const actualResult = totalLikes(listWithSingleBlog)
+        expect(actualResult).toBe(150)
+    })
+
+    const listWithMultipleBlogs = [
+        {
+            "title": "A Day in a Life of a CS Student",
+            "author": "Sergio Vieri",
+            "url": "http://sergiovieri.diary.com",
+            "likes": 150
+        },
+        {
+            "title": "Once Upon a Time",
+            "author": "Steven Hans Limantoro",
+            "url": "http://stevenlimantoro.diary.com",
+            "likes": 55
+        },
+        {
+            "title": "I Am Finally Employed!",
+            "author": "Felix Johnson",
+            "url": "http://johnson.diary.com",
+            "likes": 1000
+        },
+    ]
+    test("When the list has multiple blogs, the total likes is the sum of ", () => {
+        const actualResult = totalLikes(listWithMultipleBlogs)
+        const expectedResult = 1000 + 150 + 55
+        expect(actualResult).toBe(expectedResult)
     })
 })
