097f58f596b128a8c97f967bd9b147440ea5b0e3
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-16 11:52:53 +0700

    Added a test for empty list of blogs

--- bloglist/tests/favoriteBlog.test.js
+++ bloglist/tests/favoriteBlog.test.js
@@ -18,4 +18,43 @@ describe("Favorite Blog", () => {
         }
         expect(actualResult).toEqual(expectedResult)
     })
+
+    const listWithMultipleBlogs = [
+        {
+            "title": "A Day in a Life of a CS Student",
+            "author": "Sergio Vieri",
+            "url": "http://sergiovieri.diary.com",
+            "likes": 150
+        },
+        {
+            "title": "Once Upon a Time",
+            "author": "Steven Hans Limantoro",
+            "url": "http://stevenlimantoro.diary.com",
+            "likes": 55
+        },
+        {
+            "title": "I Am Finally Employed!",
+            "author": "Felix Johnson",
+            "url": "http://johnson.diary.com",
+            "likes": 1000
+        },
+    ]
+
+    test("When the list has multiple blogs, the most favorite blog is the blog with biggest number of likes", () => {
+        const actualResult = favoriteBlog(listWithMultipleBlogs)
+        const expectedResult = 
+        {
+            "title": "I Am Finally Employed!",
+            "author": "Felix Johnson",
+            "likes": 1000
+        }
+        expect(actualResult).toEqual(expectedResult)
+    })
+    
+    const listWithNoBlog = []
+    test("When the list has no blog, the most favorite blog is an empty object", () => {
+        const actualResult = favoriteBlog(listWithNoBlog)
+        const expectedResult = {}
+        expect(actualResult).toEqual(expectedResult)
+    })
 })
