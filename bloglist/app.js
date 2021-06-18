4e00a0c13fdc733954f00c56cecfacbcfb4fd4cb
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-18 14:35:23 +0700

    Refactored all the middlewares into app

---
+++ bloglist/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require("./controllers/blogs")
+
+const mongoUrl = "mongodb+srv://Steven:StevenHans@cluster0.s52wk.mongodb.net/Bloglist?retryWrites=true&w=majority"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/blogs", blogsRouter)
+
+module.exports = app
--- bloglist/index.js
+++ bloglist/index.js
@@ -1,17 +1,4 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require("./controllers/blogs")
-
-const Blog = require("./models/blog")
-
-const mongoUrl = "mongodb+srv://Steven:StevenHans@cluster0.s52wk.mongodb.net/Bloglist?retryWrites=true&w=majority"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-app.use("/api/blogs", blogsRouter)
+const app = require("./app")
 
 const PORT = 3003
 app.listen(PORT, () => {
