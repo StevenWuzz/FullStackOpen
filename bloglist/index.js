cefc924cf434c85aca7945a6d86bc4ed94baebde
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-18 14:28:02 +0700

    Cleaned up unnecessary codes

--- bloglist/index.js
+++ bloglist/index.js
@@ -4,14 +4,6 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require("./controllers/blogs")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-//const Blog = mongoose.model('Blog', blogSchema)
 const Blog = require("./models/blog")
 
 const mongoUrl = "mongodb+srv://Steven:StevenHans@cluster0.s52wk.mongodb.net/Bloglist?retryWrites=true&w=majority"
@@ -20,26 +12,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
-
-/*
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-*/
 
 const PORT = 3003
 app.listen(PORT, () => {
