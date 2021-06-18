cd1a7db6dc830fff8a0df59c6a2ca02c9f19936b
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-18 12:13:04 +0700

    Initiated bloglist and refactored the schema model

---
+++ bloglist/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+//const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require("./models/blog")
+
+const mongoUrl = "mongodb+srv://Steven:StevenHans@cluster0.s52wk.mongodb.net/Bloglist?retryWrites=true&w=majority"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
