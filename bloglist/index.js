fdb60f6080abdea8eebd45c844bf721deba78ced
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-18 14:17:46 +0700

    Refactored the routers

--- bloglist/index.js
+++ bloglist/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require("./controllers/blogs")
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -18,7 +19,9 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/blogs", blogsRouter)
 
+/*
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
@@ -36,6 +39,7 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+*/
 
 const PORT = 3003
 app.listen(PORT, () => {
