c7648dfd50030bda9e73601f9c4df3c1975603b2
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-19 09:34:15 +0700

    Implemented a unit test for GET request

---
+++ bloglist/tests/blog_api.test.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require("../models/blog")
+
+const initialBlog =
+[
+    {
+        title: "A Day in a Life of a CS Student",
+        author: "Steven Hans Limantoro",
+        url: "http://steven.csdiary.com",
+        likes: 150
+    },
+    {
+        title: "Good Life",
+        author: "Felix Huang",
+        url: "http://huang.blog.com",
+        likes: 50
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = initialBlog.map(blog => new Blog(blog))
+    const savePromiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(savePromiseArray)
+})
+
+describe('GET request returns the correct amount and format', () => {
+    test('Blogs are returned in the correct amount', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(2)
+    })
+    
+    test('Blogs are returned in the correct format', async () => {
+        await api.get('/api/blogs')
+        .expect('Content-Type', /application\/json/)
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
+
+
+
