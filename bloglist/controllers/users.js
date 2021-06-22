57a02db328257b677b977b12ed91f55be906f31e
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-22 15:26:23 +0700

    Handled password being too short

--- bloglist/controllers/users.js
+++ bloglist/controllers/users.js
@@ -3,14 +3,24 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
+    if(request.body.password.length < 3){
+        return response.status(400).json({error: 'Password is too short'})
+    }
+
     const passwordHash = await bcrypt.hash(request.body.password, 10)
     const newUser = new User({
         username: request.body.username,
         passwordHash,
         name: request.body.name
     })
-    const savedUser = await newUser.save()
-    response.json(savedUser)
+    try{
+        const savedUser = await newUser.save()
+        response.json(savedUser)
+    }
+    catch(exception){
+        //next(exception)
+        response.json({Error: "Cannot save the new user"})
+    }
 })
 
 module.exports = usersRouter
