da9c70a728d2ef206381908dd4bacdb19114c175
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-22 16:09:17 +0700

    Apply the unique validator for username

--- bloglist/models/user.js
+++ bloglist/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -13,6 +14,8 @@ const userSchema = new mongoose.Schema({
     },
     name: String
 })
+
+userSchema.plugin(uniqueValidator);
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
