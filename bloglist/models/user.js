133169d5ad8e9367de8abfb699de61ba8cb6b93a
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-22 15:27:00 +0700

    Added a schema model for the user

--- bloglist/models/user.js
+++ bloglist/models/user.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    passwordHash: String,
+    username: {
+      unique: true,
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    passwordHash: {
+      type: String,
+      required: true
+    },
     name: String
 })
 
