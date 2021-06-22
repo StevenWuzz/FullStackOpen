0e141bd0cec66d68a78ccd73d51e203cb7af07c2
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-06-22 19:47:13 +0700

    Modified get request to populate user info and post request to store user id

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,26 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const returnedBlogs = await Blog.find({})
+    const returnedBlogs = await Blog.find({}).populate('user')
     return response.json(returnedBlogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    if(!(request.body.title) && !(request.body.url)){
+blogsRouter.post('/', async (request, response) => {
+    body = request.body
+
+    if(!(body.title) && !(body.url)){
       return response.status(400).end()
     }
 
-    if(!request.body.likes){
+    if(!body.likes){
       request.body.likes = 0
     }
+
+    const user = await User.findById(body.userId)
     
-    const blog = new Blog(request.body)
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.string,
+      likes: body.likes,
+      user: user._id
+    })
+  
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.json(savedBlog)
   })
 
 module.exports = blogsRouter
