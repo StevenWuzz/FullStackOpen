dfd72707323eb5a8c3e5ec70480d4d8c67fc120b
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-03 11:39:37 +0700

    Only logged in users can post new blogs

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,11 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const returnedBlogs = await Blog.find({}).populate('user')
     return response.json(returnedBlogs)
 })
+
+const tokenExtractor = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith("Bearer ")){
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.post('/', async (request, response) => {
     body = request.body
@@ -16,9 +25,19 @@ blogsRouter.post('/', async (request, response) => {
 
     if(!body.likes){
       request.body.likes = 0
+    }  
+    
+    const token = tokenExtractor(request)
+    if(token === null){
+      return response.status(401).json({error: "Token can't be found"})
     }
-
-    const user = await User.findById(body.userId)
+    
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!decodedToken){
+      return response.status(401).json({error: "Token can't be verified or decoded"})
+    }
+    
+    const user = await User.findById(decodedToken.id)
     
     const blog = new Blog({
       title: body.title,
@@ -27,11 +46,12 @@ blogsRouter.post('/', async (request, response) => {
       likes: body.likes,
       user: user._id
     })
-  
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    response.json(savedBlog)
+    return response.json(savedBlog)
+    
   })
 
 module.exports = blogsRouter
