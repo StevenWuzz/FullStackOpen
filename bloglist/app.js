a76fed6541b4555a4c992aa5e070c3613784ace6
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-03 14:07:09 +0700

    Applied middleware

--- bloglist/app.js
+++ bloglist/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
+const tokenExtractor = require("./utils/middlewares")
 const mongoURL = require("./utils/config").MONGODB_URI
 
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use(tokenExtractor)
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/login", loginRouter)
