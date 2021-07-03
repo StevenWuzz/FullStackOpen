602212cd00884fd6d6e576f2676ef3da3896d456
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-03 15:11:27 +0700

    Middleware for handling errors

--- bloglist/utils/middlewares.js
+++ bloglist/utils/middlewares.js
@@ -6,4 +6,19 @@ const tokenExtractor = (request, response, next) => {
     next()
 }
 
-module.exports = tokenExtractor
+const errorHandler = (error, request, response, next) => {
+    if(error.name == "CastError") {
+        return response.status(400).json({error: "Malformatted id"})
+    }
+    else if(error.name === "ValidationError") {
+        return response.status(400).json({error: error.message})
+    }
+    else if(error.name === "JsonWebTokenError") {
+        return response.status(401).json({error: 'Invalid token'})
+    }
+}
+
+module.exports = {
+    tokenExtractor,
+    errorHandler
+}
