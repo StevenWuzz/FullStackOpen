2370df1d65f3f052bade6d5d91d321952498c81a
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-04 21:27:22 +0700

    Modified DELETE request to use userExtractor middleware

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const returnedBlogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -38,26 +37,17 @@ blogsRouter.post('/', async (request, response) => {
   })
 
 blogsRouter.delete('/:id', async(request, response) => {
-  const token = request.token
-  if(token === null){
-    return response.status(401).json({error: "Token can't be found"})
-  }
-    
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if(!decodedToken){
-    return response.status(401).json({error: "Token can't be verified or decoded"})
-  }
 
   const requestedBlogId = request.params.id
   const blog = await Blog.findById(requestedBlogId)
-
-  const requestedUserId = decodedToken.id
+  
+  const user = request.user
+  const requestedUserId = user._id
   const correctUserId = blog.user._id
 
   if(requestedUserId.toString() === correctUserId.toString()){
     await Blog.findByIdAndRemove(requestedBlogId)
 
-    const user = await User.findById(requestedUserId)
     user.blogs = user.blogs.filter(id => id === requestedBlogId)
     const newUserInfo = {
       blogs: user.blogs,
